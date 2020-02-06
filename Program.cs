224de72959ef36f42a08f3a52a401ca918b62288
Your Name <you@example.com>
2020-02-05 22:00:32 -0500

    Comments update

--- Program.cs
+++ Program.cs
@@ -1,25 +1,11 @@
-﻿using System;
-/*
- * 
-s
-
-
-PROGRAM #1: Create a C# console application that demonstrates an Array with the following instructions:
-
-1. Choose either type of array (Manually populated for Auto populated) seen in the example
-
-2. The program doesn't ask the user anything, it will just run and execute code. No user input.
-
-3. Have the program execute an array that is of size 25 elements.
-
-4. Then, have the code simply display in the Console the following 25 times (Here is a sample of the output):
- */
-//
+﻿//
 // Author: Ettienne Voges
-// Program Week 3 Array program
+// Program Week 4 Array program
 //
 // Demonstrates an Array
 //
+using System;
+
 namespace Deliverable3Arrays
 {
     class Program
